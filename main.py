1cfa9c3e6e6b5470518476546f2043eedaa6d91d
root <alexis.baudron15@gmail.com>
2020-11-24 03:05:01 +0000

    cvpr sub

--- main.py
+++ main.py
@@ -13,22 +13,30 @@ opt = options.set()
 
 print(util.magenta("reading list of sequences..."))
 seq_list = data.load_sequence_list(opt,subset=1)
-seq_list = [("02958343","eebbce8b77bdb53c82382fde2cafeb9")]
+seq_list = list(range(5, 75))
 
 with torch.cuda.device(opt.gpu):
 
-	pmo = model.Model(opt)
-	pmo.build_network(opt)
-	pmo.restore_checkpoint(opt)
+    pmo = model.Model(opt)
+    pmo.build_network(opt)
+    pmo.restore_checkpoint(opt)
 
-	print(util.yellow("======= OPTIMIZATION START ======="))
-	for c,m in seq_list:
-		pmo.load_sequence(opt,c,m)
-		pmo.setup_visualizer(opt)
-		pmo.setup_variables(opt)
-		pmo.setup_optimizer(opt)
-		pmo.time_start(opt)
-		pmo.optimize(opt)
-	print(util.yellow("======= OPTIMIZATION DONE ======="))
-	pmo.write_video(opt)
-	pmo.save_mesh(opt)
+    print(util.yellow("======= OPTIMIZATION START ======="))
+    for num in seq_list:
+        opt.name = num
+        try:
+            pmo.load_sequence(opt,num)
+        except Exception as e:
+            raise Exception(e)
+            continue
+        pmo.setup_visualizer(opt)
+        pmo.setup_variables(opt)
+        pmo.setup_optimizer(opt)
+        pmo.time_start(opt)
+        pmo.optimize(opt)
+        print('WRITING MESH')
+        pmo.save_mesh(opt)
+    print(util.yellow("======= OPTIMIZATION DONE ======="))
+    #pmo.write_video(opt)
+    
+    
